f0fbcc8ab1c1ae325ec252b29c6ff01c07acc8c7
jaredcohe <jaredcohe@gmail.com>
2016-05-03 12:54:30 -0400

    updates based on meeting with sydney and ami

--- runner.rb
+++ runner.rb
@@ -9,16 +9,23 @@ require 'pp'
 ## add rest of pages
 
 # get the data
-source_url = "https://www.teacherspayteachers.com/Browse/Search:pete+the+cat/Page:2"
-doc = Nokogiri::HTML(open(source_url))
+# page 1 = https://www.teacherspayteachers.com/Browse/Search:pete%20the%20cat
 
-# get anchor link objects
-anchor_link_object_list = doc.xpath("//div[@id='browse_list']//div[@class='title']//a")
-
-# make array of URL list
 product_url_list = []
-anchor_link_object_list.each do |anchor_link_object|
-  product_url_list << anchor_link_object['href']
+
+(1..10).each do |i|
+
+  source_url = "https://www.teacherspayteachers.com/Browse/Search:pete+the+cat/Page:#{i}"
+  doc = Nokogiri::HTML(open(source_url))
+
+  # get anchor link objects
+  anchor_link_object_list = doc.xpath("//div[@id='browse_list']//div[@class='title']//a")
+
+  # make array of URL list
+  anchor_link_object_list.each do |anchor_link_object|
+    product_url_list << anchor_link_object['href']
+  end
+
 end
 
 # print to file
